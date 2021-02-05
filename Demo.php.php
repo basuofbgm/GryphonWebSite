3be91e70451fe4b67e7ce319d901106d270bba31
BASANAGOUDA PATIL <47344640+basuofbgm@users.noreply.github.com>
2021-02-05 22:00:19 +1100

    Add files via upload

---
+++ Demo.php.php
@@ -0,0 +1,37 @@
+<?php
+
+define('DB_NAME', 'gryphonsystem');
+
+define('DB_USER', 'root');
+
+define('DB_PASSWORD', 'admin');
+
+define('DB_HOST', 'localhost');
+
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+if (!$link)
+ {
+
+     die('Could not connect: ' . mysql_error());
+   
+ }
+$db_selected = mysql_select_db(DB_NAME, $link);
+
+if (!$db_selected) 
+{
+
+     die('Can\'t use ' . DB_NAME . ': ' . mysql_error());
+
+}
+$value1 = $_POST['FullName'];
+
+$value2 = $_POST['PhoneNumber'];
+
+$value3 = $_POST['EmailID'];
+$value4 = $_POST['Message'];
+$sql = "INSERT INTO gryphonsystem.contactus(FullName, PhoneNumber, EmailID,Message ) VALUES ('$value1', '$value2', '$value3','$value4')";
+$result = mysql_query($sql);
+mysql_close();
+
+/>
